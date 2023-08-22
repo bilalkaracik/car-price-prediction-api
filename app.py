990fc54cce006ce085bbb2fa526f384b87bb9f5e
Bilal KARACIK <116632349+bilalkaracik@users.noreply.github.com>
2023-08-22 15:09:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import joblib
+import pandas as pd
 from flask import Flask, request, jsonify
-from xgboost import XGBRegressor
+import numpy as np
 
 app = Flask(__name__)
 
@@ -20,16 +21,27 @@ def root():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        data = request.get_json()
-        # Gelen veriyi modele uygun hale getirin
-        # Örnek: text = data['text']
-        # dtest = xgb.DMatrix([[text]])
+        csv_data = request.files['file']  # CSV dosyasını al
+        input_data = pd.read_csv(csv_data)  # CSV verisini DataFrame'e dönüştür
         
-        # Tahmin
-        prediction = model_service.predict(dtest)
-
+        # # Gerekli dönüşümleri ve One-Hot Encoding işlemlerini yapma
+        # input_data["doornumber"] = input_data["doornumber"].replace({"two": 2, "four": 4})
+        # input_data['cylindernumber'] = input_data['cylindernumber'].replace({'four': 4, 'six': 6, 'five': 5,
+        #                                                                      'three': 3, 'twelve': 12, 'two': 2,
+        #                                                                      'eight': 8})
+        # dums = pd.get_dummies(input_data[["CarName", "fueltype", "aspiration", "carbody",
+        #                                   "drivewheel", "enginelocation", "enginetype",
+        #                                   "fuelsystem"]])
+        # X_ = input_data.drop(["price" ,"CarName", "fueltype", "aspiration", "carbody", "drivewheel",
+        #                       "enginelocation", "enginetype", "fuelsystem"], axis=1)
+        # X = pd.concat([X_, dums], axis=1)
+        input_np = input_data.to_numpy()
+        
+        # Tahmin işlemi
+        prediction = model_service.predict(input_np)
+        
         response = {
-            'predicted_label': prediction
+            'predicted_price': prediction.tolist()
         }
 
         return jsonify(response)
@@ -38,4 +50,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)

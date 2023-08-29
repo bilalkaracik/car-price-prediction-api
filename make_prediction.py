d386dd14a657e07847980ec9520b83de1cf70c9b
Bilal KARACIK <116632349+bilalkaracik@users.noreply.github.com>
2023-08-29 11:27:37 +0300

    Update make_prediction.py

--- make_prediction.py
+++ make_prediction.py
@@ -1,9 +1,16 @@
 import requests
+import pandas as pd
 
-file_path = r"C:*************\flask/sample.csv"
+# CSV dosyasının yolunu belirtin
+file_path = r"*******\sample.csv"
 url = "http://127.0.0.1:5000/predict"
 
-files = {'file': open(file_path, 'rb')}
-response = requests.post(url, files=files)
+# CSV dosyasını DataFrame olarak okuyun
+csv_data = pd.read_csv(file_path)
+
+# DataFrame'i JSON formatına dönüştürün
+json_data = csv_data.to_dict(orient='list')
+
+response = requests.post(url, json=json_data)
 
 print(response.json())

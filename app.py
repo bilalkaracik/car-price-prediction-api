cc664fc08dbf7b41a439529b3703b187a86c9716
Bilal KARACIK <116632349+bilalkaracik@users.noreply.github.com>
2023-08-29 11:27:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,24 +21,12 @@ def root():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        csv_data = request.files['file']  # CSV dosyasını al
-        input_data = pd.read_csv(csv_data)  # CSV verisini DataFrame'e dönüştür
+        input_json = request.json  # JSON verisini al
         
-        # # Gerekli dönüşümleri ve One-Hot Encoding işlemlerini yapma
-        # input_data["doornumber"] = input_data["doornumber"].replace({"two": 2, "four": 4})
-        # input_data['cylindernumber'] = input_data['cylindernumber'].replace({'four': 4, 'six': 6, 'five': 5,
-        #                                                                      'three': 3, 'twelve': 12, 'two': 2,
-        #                                                                      'eight': 8})
-        # dums = pd.get_dummies(input_data[["CarName", "fueltype", "aspiration", "carbody",
-        #                                   "drivewheel", "enginelocation", "enginetype",
-        #                                   "fuelsystem"]])
-        # X_ = input_data.drop(["price" ,"CarName", "fueltype", "aspiration", "carbody", "drivewheel",
-        #                       "enginelocation", "enginetype", "fuelsystem"], axis=1)
-        # X = pd.concat([X_, dums], axis=1)
+        input_data = pd.DataFrame(input_json)  # JSON verisini DataFrame'e dönüştür
         input_np = input_data.to_numpy()
         
-        # Tahmin işlemi
-        prediction = model_service.predict(input_np)
+        prediction = model_service.predict(input_np)  # Tahmin işlemi
         
         response = {
             'predicted_price': prediction.tolist()
